c7173da2813aa7ee8cef0cf96e57f46217c93ec3
scott@lubuntu <scozv@users.noreply.github.com>
2015-09-26 19:50:45 +0800

    enviroment script for 1st time

---
+++ 0.sh
@@ -0,0 +1,8 @@
+# Building the environment for only 1st time
+
+# Ruby for Jekyll
+# alternative way https://gorails.com/setup/ubuntu/15.04
+sudo apt-get install -y ruby-dev
+sudo gem update
+sudo gem install bundler
+bundle install
