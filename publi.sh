4d40fac203a7973e4fea16cdb1a7e1c7a7931c6d
scott@lubuntu <scozv@yandex.com>
2016-05-08 13:56:23 +0800

    see scotv/scotv.github.com#16 update publi.sh

--- publi.sh
+++ publi.sh
@@ -1,10 +1,13 @@
 # https://github.com/untra/polyglot/wiki/Github-Pages-Support
+git branch -D _site
+git branch _site
 git checkout _site
-git rebase master
 rm -rf _site
 bundle exec jekyll build
 git add -f _site
 git commit -m "Polyglot build on $(date +"%y%m%d%H%M")"
 git push -f origin _site
-git subtree push -f --prefix  _site/ origin gh-pages
+git branch -D gh-pages
+git push origin :gh-pages
+git subtree push --prefix  _site/ origin gh-pages
 git checkout master
